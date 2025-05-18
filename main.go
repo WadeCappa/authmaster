5b1f2f1d15c4c07e8857f5a1e64a9b241a1b132c
Wade Cappa <wade.cappa@msn.com>
2025-05-18 09:03:44 -0400

    adding protobuf

--- main.go
+++ main.go
@@ -6,14 +6,23 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+const AUTH_PREFIX = "Authorization"
+
 func setupRouter() *gin.Engine {
 	r := gin.Default()
 
 	r.GET("/test", func(c *gin.Context) {
-		c.String(http.StatusOK, "pong")
+		token := c.Request.Header[AUTH_PREFIX]
+		c.JSON(http.StatusOK, gin.H{
+			"token_data": c.Request.Header["Authorization"],
+		})
 	})
 
 	return r
+}
+
+func getToken(c *gin.Context) string {
+	return c.Request.Header[AUTH_PREFIX]
 }
 
 func main() {
---
+++ store/store.go
@@ -0,0 +1,17 @@
+package store
+
+import (
+	"context"
+	"os"
+
+	"github.com/jackc/pgx/v5"
+)
+
+func Connect[T any](query func(*pgx.Conn) *T) (*T, error) {
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close(context.Background())
+	return query(conn), nil
+}
