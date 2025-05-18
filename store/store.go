08ce72390a81c113b971f269cebee450d16d921b
Wade Cappa <wade.cappa@msn.com>
2025-05-18 13:53:19 -0400

    adding scripts

--- store/store.go
+++ store/store.go
@@ -8,6 +8,7 @@ import (
 )
 
 func Call[T any](query func(*pgx.Conn) T) (*T, error) {
+	// this is probably insecure. Will want to change how we access this in the future
 	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
 	if err != nil {
 		return nil, err
---
+++ tests/run-for-testing.sh
@@ -0,0 +1,2 @@
+
+DATABASE_URL=postgres://postgres:pass@localhost:5432/authmaster_db go run main.go
